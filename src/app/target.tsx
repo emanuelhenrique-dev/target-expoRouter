90d153ae6dc2042dc6db29b3f956074a4d230016
Emanuel Henrique <emanuelhenriquefs@gmail.com>
2026-02-11 17:39:50 -0300

    create Input component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,6 +1,7 @@
+import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
 import { PageHeader } from '@/components/PageHeader';
-import { router } from 'expo-router';
-import { Button, StatusBar, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
 export default function Target() {
   return (
@@ -16,7 +17,14 @@ export default function Target() {
           }}
         />
       </View>
-      <Button title="Voltar" onPress={() => router.back()} />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input
+          label="Nova meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+        />
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
